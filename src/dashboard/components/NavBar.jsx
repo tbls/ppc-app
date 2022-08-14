1a2509787eec65d46420e5d6e5850d44520c0b99
tbls <tbls.0312@gmail.com>
2022-08-14 10:45:05 -0500

    mejora NavBar

--- src/dashboard/components/NavBar.jsx
+++ src/dashboard/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,32 +9,40 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>
-          <Link to="/">SuraApp </Link>
+        <Navbar.Brand as={Link} to="/">
+          PPC
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <Nav.Link as={Link} to="/">
+              Pendientes Por Condición
+            </Nav.Link>
+
+            <Nav.Link href="#pricing">Emisiones Figitivas</Nav.Link>
+
+            <Nav.Link href="#pricing">Equipos Principales</Nav.Link>
+
+            {/* <NavDropdown
+              title="Pendientes Por Condición"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Pendientes</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Superados</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Cancelados</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+          <Nav className="d-flex align-items-end align-items-lg-center">
+            <span className="text-info me-3 sm">Tommy López</span>
+            <Button variant={'outline-primary'} className="mt-1 mt-lg-0">
+              Salir
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
